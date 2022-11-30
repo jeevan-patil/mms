43fbb73638dcd4b7892ddd62392fc1f668661aca
Jeevan Patil <jeevanpaatil@gmail.com>
2022-11-30 21:55:15 +0800

    added file poller, transformer and writer

--- src/main/java/com/nexfincorp/mms/config/MandateInboundMessageProcessorConfig.java
+++ src/main/java/com/nexfincorp/mms/config/MandateInboundMessageProcessorConfig.java
@@ -1,5 +1,6 @@
 package com.nexfincorp.mms.config;
 
+import com.nexfincorp.mms.processor.mandate.MandateInboundFileProcessor;
 import java.io.File;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
-import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.filters.CompositeFileListFilter;
@@ -16,13 +17,16 @@ import org.springframework.integration.file.filters.SimplePatternFileListFilter;
 
 @Log4j2
 @Configuration
-public class MandateMessageSourceConfig {
+public class MandateInboundMessageProcessorConfig {
 
   @Value("${mandate.pattern.inbound}")
   private String mandateInboundFilePattern;
 
   @Autowired
   private MandateFileLocations mandateFileLocations;
+
+  @Autowired
+  private MandateInboundFileProcessor mandateInboundFileProcessor;
 
   private CompositeFileListFilter<File> mandateFileFilterPattern() {
     final CompositeFileListFilter<File> filter = new CompositeFileListFilter<>();
@@ -31,17 +35,18 @@ public class MandateMessageSourceConfig {
   }
 
   @Bean
-  @InboundChannelAdapter(value = "mandateInboundFileChannel", poller = @Poller(fixedDelay = "5000"))
+  @InboundChannelAdapter(value = "mandateInboundFileChannel", poller = @Poller(fixedDelay = "1000"))
   public FileReadingMessageSource fileReadingMessageSource() {
     log.info("Created mandate file inbound message channel");
     final FileReadingMessageSource reader = new FileReadingMessageSource();
+    reader.setAutoCreateDirectory(true);
     reader.setDirectory(mandateFileLocations.inboundDirectory());
     reader.setFilter(mandateFileFilterPattern());
     return reader;
   }
 
   @Bean
-  @ServiceActivator(inputChannel = "mandateInboundFileChannel")
+  //@ServiceActivator(inputChannel = "mandateInboundFileChannel")
   public FileWritingMessageHandler fileWritingMessageHandler() {
     log.info("Created mandate file outbound message channel");
     final FileWritingMessageHandler writer = new FileWritingMessageHandler(
@@ -49,5 +54,13 @@ public class MandateMessageSourceConfig {
     writer.setAutoCreateDirectory(true);
     writer.setExpectReply(false);
     return writer;
+  }
+
+  @Bean
+  public IntegrationFlow flowOnReturnOfMessage() {
+    return IntegrationFlow.from("mandateInboundFileChannel")
+        .transform(mandateInboundFileProcessor, "process")
+        .handle(fileWritingMessageHandler())
+        .get();
   }
 }
---
+++ src/main/java/com/nexfincorp/mms/processor/mandate/MandateInboundFileProcessor.java
@@ -0,0 +1,40 @@
+package com.nexfincorp.mms.processor.mandate;
+
+import com.nexfincorp.mms.config.MandateFileLocations;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component(value = "mandateInboundFileProcessor")
+public class MandateInboundFileProcessor {
+
+  @Autowired
+  private MandateFileLocations mandateFileLocations;
+
+  public String process(final Message<File> inboundFile) {
+    final String fileName = inboundFile.getPayload().getName();
+    log.info("Received mandate file : " + fileName);
+
+    final String fileNameWithPath =
+        Objects.requireNonNull(inboundFile.getHeaders().get("file_originalFile")).toString();
+    final Path inboundFilePath = Paths.get(fileNameWithPath);
+
+    try {
+      Files.move(inboundFilePath,
+          Paths.get(mandateFileLocations.processedDirectory() + "/" + fileName));
+      log.info("Moved " + fileName + " to processed directory");
+    } catch (IOException e) {
+      log.error("Exception occurred while moving file to processed directory.");
+    }
+
+    return inboundFile.getPayload().getName();
+  }
+}
